569fbc03f285aeb98f928e5299fb7e34e970abbb
Sarah Lawrence <s.j.lawrence8@gmail.com>
2014-10-26 18:25:24 +1100

    styling for view role

--- view/viewRole.php
+++ view/viewRole.php
@@ -1,8 +1,12 @@
 <html>
 	<head>
-		<script src="/SDP/view/js/jquery-1.11.1.min.js"></script>
-		<script src="/SDP/view/js/jquery-1.11.1.min.js"></script>
-<script type="text/javascript">
+		<title>BeanSprouts - View Role</title>
+		<link href='http://fonts.googleapis.com/css?family=Patrick+Hand+SC' rel='stylesheet' type='text/css'>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="view/css/main.min.css">
+		<script src="view/js/jquery-1.11.1.min.js"></script>
+		<script src="view/js/script.js"></script>
+		<script type="text/javascript">
 			$(document).ready(function() 
 			{
 
@@ -45,70 +49,95 @@
 
 			});						
 		</script>
+		<script>
+			$(document).ready(function() {
+			    dateFormat("#date1")
+			});
+		</script>
 	</head>
+
+
+
+
 	<body>
         <?php include("menu.php"); ?>
         <?php include("./model/isActive.php"); ?>
-		<div id="roleID">Role ID: <?php echo($row['roleID']); ?></div> <br>
-		<div id="name">Role Name: <?php echo($row['name']); ?></div> <br>
-		<div id="paid">Paid: <?php if($row['isPaid'] == 1){echo("Yes");}else{echo("No");} ?></div> <br>
-		<div id="rate"> Rate: <?php echo($row['rate']); ?></div> <br>
-		<div id="description">Role Description: <?php echo($row['description']); ?></div> <br>
-		<div id="location">Role Requirements <?php echo($row['requirements']); ?></div> <br>
-		<div id="startDate">Role Date: <?php echo($row1['date']); ?></div> <br>
-		<div id="startTime">Role Start Time: <?php echo($row1['startTime']); ?></div> <br>
-		<div id="endTime">Role End Time: <?php echo($row1['endTime']); ?></div> <br>
-		
-		<?php 
-        //MODIFY THE LINK FOR DELETE,UPDATE, & REGISTER USING AJAX
-        if(!empty($_SESSION))
-        if($_SESSION['userType'] == 2){ ?>
-        <a id="deleteRole" href="/SDP/model/deleteRoleCheck.php?roleid=<?php echo($row['roleID']); ?>">Delete</a>
-        <a href="/SDP/updateRole.php?roleid=<?php echo($row['roleID']); ?>">Update</a>
-        <div id="deleteRoleMessage"></div>
-        <?php } ?>
 
-		<?php
-			//IF the person who made this Role is the person looking at it, Echo out every applicant and allow and a button to select them for the role.
-			$sql2 = "SELECT managerID from event WHERE eventID =  (SELECT eventID FROM activity WHERE activityID = (SELECT activityID FROM role WHERE roleID = "  . $row['roleID'] . "))";
-			$result2 = mysqli_query($db, $sql2);
-			$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
-			if(!empty($_SESSION))
-			if($_SESSION['userID'] == $row2['managerID']) //If you are the manager who made this event
-			{
-				echo("<h1>Applicant List</h1>");
-				$sql3 = "SELECT * FROM application WHERE roleID = " . $row['roleID']." AND status = 0"; //Only display the status 0
-				$result3 = mysqli_query($db, $sql3);
+        <div id="wrap">
+        	<div class="role-details">
+				<div id="name"><?php echo($row['name']); ?></div>
 
-				while($row3 = mysqli_fetch_array($result3)) 
-				{
-					$sql4 = "SELECT firstName, lastName FROM user WHERE userID = " . $row3['userID'];
-					$result4 = mysqli_query($db, $sql4);
-					$row4 = mysqli_fetch_array($result4,MYSQLI_ASSOC);
-					echo $row3['userID'] . " " .$row4['firstName'] . " " . $row4['lastName'] . " Date Submitted " . $row3['dateSubmitted'] . " <a href = \"/SDP/model/approve.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">Approve</a>" ." - <a href = \"/SDP/model/ignore.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">Ignore</a><br>";
-				}
-                
-                echo("<h1>Approve List</h1>");
-				$lqs3 = "SELECT * FROM application WHERE roleID = " . $row['roleID']." AND status = 1"; //Only display the status 0
-				$result3 = mysqli_query($db, $lqs3);
+				<div id="startDate"><i class="fa fa-calendar"></i> <span id="date1"><?php echo($row1['date']); ?></span></div>
+				<div id="startTime"><span class="role-time"><i class="fa fa-clock-o"></i> Starts <?php echo($row1['startTime']); ?></span></div>
+				<div id="endTime"><span class="role-time"><i class="fa fa-clock-o"></i> Ends <?php echo($row1['endTime']); ?></span></div>
 
-				while($row3 = mysqli_fetch_array($result3)) 
-				{
-					$sql4 = "SELECT firstName, lastName FROM user WHERE userID = " . $row3['userID'];
-					$result4 = mysqli_query($db, $sql4);
-					$row4 = mysqli_fetch_array($result4,MYSQLI_ASSOC);
-					echo $row3['userID'] . " " .$row4['firstName'] . " " . $row4['lastName'] . " Date Submitted " . $row3['dateSubmitted'] . " <a href = \"/SDP/model/deAllocate.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">De-Allocate</a>";
-				}
-                
-			}
-			elseif ($row['isPaid'] == 1 && $_SESSION['userType'] == 1) //the role is paid and the user is a sprout 
-			{
-				echo("<a href = \"/SDP/model/applyRole.php?roleid=" . $row['roleID'] . "\"> Apply </a> ");
-			}
-			elseif ($row['isPaid'] == 0 && $_SESSION['userType'] < 2) //the role is non-paid and the user is a sprout or volunteer 
-			{
-				echo("<a href = \"/SDP/model/applyRole.php?roleid=" . $row['roleID'] . "\"> Apply </a> ");
-			}
-		?>
+				<div id="paid">Paid: <?php if($row['isPaid'] == 1){echo("Yes");}else{echo("No");} ?><div id="rate"> Rate: $<?php echo($row['rate']); ?> / hr</div></div>
+				<div id="requirements">Requirements: <?php echo($row['requirements']); ?></div>
+				<div id="description">Description: <?php echo($row['description']); ?></div>
+				
+			
+
+				<div class="role-edit">
+					<?php 
+			        //MODIFY THE LINK FOR DELETE,UPDATE, & REGISTER USING AJAX
+			        if(!empty($_SESSION))
+			        if($_SESSION['userType'] == 2){ ?>
+
+			    	<a href="/SDP/updateRole.php?roleid=<?php echo($row['roleID']); ?>"><i class="fa fa-pencil-square-o"></i> Edit Role</a><br>
+			        <a id="deleteRole" href="/SDP/model/deleteRoleCheck.php?roleid=<?php echo($row['roleID']); ?>"><i class="fa fa-trash-o"></i> Delete Role</a>
+			        
+			        <div id="deleteRoleMessage"></div>
+			        <?php } ?>
+			    </div>	
+
+			</div>
+			
+			<div class="role-applicant">
+
+				<?php
+					//IF the person who made this Role is the person looking at it, Echo out every applicant and allow and a button to select them for the role.
+					$sql2 = "SELECT managerID from event WHERE eventID =  (SELECT eventID FROM activity WHERE activityID = (SELECT activityID FROM role WHERE roleID = "  . $row['roleID'] . "))";
+					$result2 = mysqli_query($db, $sql2);
+					$row2 = mysqli_fetch_array($result2,MYSQLI_ASSOC);
+					if(!empty($_SESSION))
+					if($_SESSION['userID'] == $row2['managerID']) //If you are the manager who made this event
+					{
+						echo("<div class=\"role-applicant-head\">Applicant List</div>");
+						$sql3 = "SELECT * FROM application WHERE roleID = " . $row['roleID']." AND status = 0"; //Only display the status 0
+						$result3 = mysqli_query($db, $sql3);
+
+						while($row3 = mysqli_fetch_array($result3)) 
+						{
+							$sql4 = "SELECT firstName, lastName FROM user WHERE userID = " . $row3['userID'];
+							$result4 = mysqli_query($db, $sql4);
+							$row4 = mysqli_fetch_array($result4,MYSQLI_ASSOC);
+							echo $row3['userID'] . " " .$row4['firstName'] . " " . $row4['lastName'] . " Date Submitted " . $row3['dateSubmitted'] . " <a href = \"/SDP/model/approve.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">Approve</a>" ." - <a href = \"/SDP/model/ignore.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">Ignore</a><br>";
+						}
+		                
+		                echo("<div class=\"role-applicant-head\">Approve List</div>");
+						$lqs3 = "SELECT * FROM application WHERE roleID = " . $row['roleID']." AND status = 1"; //Only display the status 0
+						$result3 = mysqli_query($db, $lqs3);
+
+						while($row3 = mysqli_fetch_array($result3)) 
+						{
+							$sql4 = "SELECT firstName, lastName FROM user WHERE userID = " . $row3['userID'];
+							$result4 = mysqli_query($db, $sql4);
+							$row4 = mysqli_fetch_array($result4,MYSQLI_ASSOC);
+							echo $row3['userID'] . " " .$row4['firstName'] . " " . $row4['lastName'] . " Date Submitted " . $row3['dateSubmitted'] . " <a href = \"/SDP/model/deAllocate.php?appid=".$row3['userID']."&roleid=".$row1['roleID']."\">De-Allocate</a>";
+						}
+		                
+					}
+					elseif ($row['isPaid'] == 1 && $_SESSION['userType'] == 1) //the role is paid and the user is a sprout 
+					{
+						echo("<a href = \"/SDP/model/applyRole.php?roleid=" . $row['roleID'] . "\"> Apply </a> ");
+					}
+					elseif ($row['isPaid'] == 0 && $_SESSION['userType'] < 2) //the role is non-paid and the user is a sprout or volunteer 
+					{
+						echo("<a href = \"/SDP/model/applyRole.php?roleid=" . $row['roleID'] . "\"> Apply </a> ");
+					}
+				?>
+			</div>
+
+		</div>
 	</body>
 </html>
