75bb59bccd462f7e7b849d7923ddf0a87ec82650
Sarah Lawrence <s.j.lawrence8@gmail.com>
2014-10-26 16:52:38 +1100

    styling for view event

--- view/viewEvent.php
+++ view/viewEvent.php
@@ -69,59 +69,64 @@
 				<div id="name"><?php echo($row['name']); ?></div>
 			</div>
 
-			<div id="container">
-				
-				<div id="startDate" class="date"><i class="fa fa-calendar"></i> Event Starts: <span id="date1"><?php echo($row['startDate']); ?></span></div>
-				<div id="endDate" class="date"><i class="fa fa-calendar"></i> Event Ends: <span id="date2"><?php echo($row['endDate']); ?></span></div>
+			<div class="event-details">
+				<div id="startDate" class="date"><i class="fa fa-calendar"></i> Starts <span id="date1"><?php echo($row['startDate']); ?></span></div>
+				<div id="endDate" class="date"><i class="fa fa-calendar"></i> Ends <span id="date2"><?php echo($row['endDate']); ?></span></div>
 				<div id="location"><?php echo($row['location']); ?></div>
 
 				<div id="description"><?php echo($row['description']); ?></div>
+
+				<div class="event-edit">
+					<?php 
+					//MODIFY THE LINK FOR DELETE,UPDATE, & REGISTER USING AJAX
+					if(!empty($_SESSION))
+					if($_SESSION['userType'] == 2){ ?>	
+					<a href="/SDP/updateEvent.php?id=<?php echo($row['eventID']); ?>"><i class="fa fa-pencil-square-o"></i> Edit Event</a><br>
+					<a id="deleteEvent" href="/SDP/model/deleteEventCheck.php?eventid=<?php echo($row['eventID']); ?>"><i class="fa fa-trash-o"></i> Delete Event</a><br>
+					<div id="deleteEventMessage"></div>
+					<?php } ?>
+					<?php if(!empty($_SESSION))if($_SESSION['userType'] == 0 || $_SESSION['userType'] == 1 ){ ?>
+					<a href="/SDP/createEvents.php">Register</a>
+					<?php } 
+					?>
+				</div>
 			</div>
-        </div>
         
-        <?php 
-        //MODIFY THE LINK FOR DELETE,UPDATE, & REGISTER USING AJAX
-        if(!empty($_SESSION))
-        if($_SESSION['userType'] == 2){ ?>
-        <a id="deleteEvent" href="/SDP/model/deleteEventCheck.php?eventid=<?php echo($row['eventID']); ?>">Delete</a>
-        <a href="/SDP/updateEvent.php?id=<?php echo($row['eventID']); ?>">Update</a>
-        <div id="deleteEventMessage"></div>
-        <?php } ?>
-        <?php if(!empty($_SESSION))if($_SESSION['userType'] == 0 || $_SESSION['userType'] == 1 ){ ?>
-        <a href="/SDP/createEvents.php">Register</a>
-        <?php } 
-        ?>
+			<div class="event-activity">
+				<div class="event-activity-head">Activities</div>
 
-        
-        <h1>Activities</h1>
-		<?php 
-			$sql = "SELECT * FROM activity WHERE eventID = " . $row['eventID'];
-			$result = mysqli_query($db, $sql);
-			while($row1 = mysqli_fetch_array($result))
-			{
-				if($row1['isActive'] == 1)
-				{
-				$link11 = "<a href=viewActivity.php?activityid=";
-				$link22 = $row1['activityID'];
-				$link33= "&managerid="; 
-				$link44 = ">View Activity</a>";
-				echo($row1['name'] . " " . $link11 . $link22 .  $link33 . $row['managerID'] .  $link44 . "<br><br>" );
-				}
-			}
-		?>
-		<?php 
-			if(!empty($_SESSION))
-			if ($_SESSION['userID'] == $row['managerID']) 
-			{
+				<?php 
+					$sql = "SELECT * FROM activity WHERE eventID = " . $row['eventID'];
+					$result = mysqli_query($db, $sql);
+					while($row1 = mysqli_fetch_array($result))
+					{
+						if($row1['isActive'] == 1)
+						{
+							$link11 = "<a href=viewActivity.php?activityid=";
+							$link22 = $row1['activityID'];
+							$link33= "&managerid="; 
+							$link44 = "> &nbsp; <i class=\"fa fa-share\"></i> View Activity</a>";
+							echo($row1['name'] . " " . $link11 . $link22 .  $link33 . $row['managerID'] .  $link44 . "<br><br>" );
+						}
+					}
+				?>
+
+				<?php 
+					if(!empty($_SESSION))
+					if ($_SESSION['userID'] == $row['managerID']) 
+					{
+						
+						$link1 = "<div class=\"event-activity-edit\"><a href=createActivity.php?eventid="; 
+						$link2 = "&managerid=";
+						$link3= "><i class=\"fa fa-plus\"></i> Add Activity</a></div>";
+						echo($link1.$row['eventID'].$link2.$row['managerID'].$link3);
+
+					}
+				?>
 				
-				$link1 = "<a href=createActivity.php?eventid="; 
-				$link2 = "&managerid=";
-				$link3= ">Create Activity</a>";
-				echo($link1.$row['eventID'].$link2.$row['managerID'].$link3);
-
-			}
-		?>
-        
-	</body>
+			</div>        
+        </div>
 	</body>
 </html>
+
+
