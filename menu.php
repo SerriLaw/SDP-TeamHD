b4192e4b722d88f5581bcad166f02e2c521b7d76
Sarah Lawrence <s.j.lawrence8@gmail.com>
2014-10-26 15:51:26 +1100

    nav bar styling and icons

--- menu.php
+++ menu.php
@@ -1,12 +1,54 @@
-        <a href="/SDP/home.php">View Events</a>
-        <?php if(isset($_SESSION['userType']) && ($_SESSION['userType'] == 2)){ ?>
-        <a href="/SDP/createEvent.php">Add Events</a>
+<link href='http://fonts.googleapis.com/css?family=Patrick+Hand+SC' rel='stylesheet' type='text/css'>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="view/css/main.min.css">
+<script src="view/js/jquery-1.11.1.min.js"></script>
+<script src="view/js/script.js"></script>
 
-        <?php } ?>
-        <?php if(isset($_SESSION['userType'])){ ?>
-        <a href="/SDP/viewProfile.php">View Profile</a>
-        <a href="/SDP/model/logout.php">Logout</a>
-        <?php }else{?>
-        <a href="/SDP/login.php">Login</a>
-        <a href="/SDP/register.php">Sign Up</a>
-        <?php }?>
+
+
+<div id="nav-bar">
+        <span id="ham"><a href="#" onClick="$('.h').toggle()"><i class="fa fa-bars"></i></a></span>
+        <ul>
+                <li><a href="/SDP/home.php"><img src="view/img/icon.png" alt="BeanSprouts"><span id="logo">BeanSprouts</span></a></li>
+                <li><a href="/SDP/home.php"><i class="fa fa-tasks"></i> View Events</a></li>
+                
+                
+
+
+                <?php if(isset($_SESSION['userType'])){ ?>
+                        <li class="h"><a href="/SDP/viewProfile.php"><i class="fa fa-user"></i> View Profile</a></li>
+
+                        <?php if(isset($_SESSION['userType']) && ($_SESSION['userType'] == 2)){ ?>
+                                <li class="h"><a href="/SDP/createEvent.php"><i class="fa fa-plus"></i> Add Events</a></li>
+
+                        <?php } ?>
+
+                        <li class="h"><a href="/SDP/model/logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+
+                <?php }else{?>
+
+                        <li class="h"><a href="/SDP/login.php"><i class="fa fa-sign-in"></i> Login</a></li>
+                        <li class="h"><a href="/SDP/register.php"><i class="fa fa-users"></i> Sign Up</a></li>
+
+                <?php }?>
+        </ul>                   
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
